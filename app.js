e19350e50c0661fb45845fce3426038f8e7c993a
Sharique Anjum <shariqueanjum05@gmail.com>
2023-07-19 18:23:31 +0530

    port change

--- app.js
+++ app.js
@@ -175,8 +175,9 @@ app.use((err,req,res,next)=>{
 
 })
 
+const port = process.env.PORT || 3000;
 
 
-app.listen(3000,()=>{
-    console.log('on port 3000')
+app.listen(port,()=>{
+    console.log(`on port ${port}`);
 })
